079165b5929a014f2bf7eda7a0ca1861e5d4cb0b
Robinson de la Hoz <rahoz@indracompany.com>
2026-08-05 16:57:11 -0500

    test: ejecucion exitosa CI

--- sessions/week-01-kickoff/src/test/java/com/indra/logistics/TrackingIdGeneratorTest.java
+++ sessions/week-01-kickoff/src/test/java/com/indra/logistics/TrackingIdGeneratorTest.java
@@ -12,6 +12,7 @@ class TrackingIdGeneratorTest {
     @DisplayName("El ID generado debe tener el formato ORIG-DEST-XXXXXXXX")
     void shouldGenerateIdWithCorrectFormat() {
         // TODO: implementar
+        // Prueba ejecucion pipeline CI
         fail("Test no implementado");
     }
 
