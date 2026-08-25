a939fd035c12d249268408de35d8bcab37ae748f
Daniel Yepes <dyepes@mysanitas.com>
2026-08-25 13:02:21 -0500

    feat: implementacion de metodo generate TrackIdGenerator

--- sessions/week-01-kickoff/src/main/java/com/indra/logistics/TrackingIdGenerator.java
+++ sessions/week-01-kickoff/src/main/java/com/indra/logistics/TrackingIdGenerator.java
@@ -1,15 +1,34 @@
 package com.indra.logistics;
 
+import java.util.Random;
+
 public class TrackingIdGenerator {
 
+
+    private Random random = new Random();
     /**
      * Genera un ID de seguimiento con formato ORIG-DEST-XXXXXXXX
      * @param origin  código de origen (ej: "BOG")
      * @param destination código de destino (ej: "MED")
      * @return ID único de seguimiento
+     * @throws Exception 
      */
-    public String generate(String origin, String destination) {
-        // TODO: implementar
-        throw new UnsupportedOperationException("Not implemented yet");
+    public String generate(String origin, String destination) throws Exception {
+    	if(origin == null || destination == null) {
+    		throw new IllegalArgumentException("fields cannot be null");
+    	}
+    	String random = generateRandom(8);
+    	return origin.concat("-").concat(destination).concat("-").concat(random);
+    }
+    
+    private String generateRandom(int reqLength) {
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        StringBuilder reqString = new StringBuilder();
+
+        for (int i = 0; i < reqLength; i++) {
+            int index = random.nextInt(alphabet.length());
+            reqString.append(alphabet.charAt(index));
+        }
+        return reqString.toString();
     }
 }
--- sessions/week-01-kickoff/src/test/java/com/indra/logistics/TrackingIdGeneratorTest.java
+++ sessions/week-01-kickoff/src/test/java/com/indra/logistics/TrackingIdGeneratorTest.java
@@ -1,8 +1,12 @@
 package com.indra.logistics;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class TrackingIdGeneratorTest {
 
@@ -10,15 +14,32 @@ class TrackingIdGeneratorTest {
 
     @Test
     @DisplayName("El ID generado debe tener el formato ORIG-DEST-XXXXXXXX")
-    void shouldGenerateIdWithCorrectFormat() {
-        // TODO: implementar
-        fail("Test no implementado");
+    void shouldGenerateIdWithCorrectFormat() throws Exception {
+    	String idGen = generator.generate("BOG", "MED");
+    	
+    	assertTrue(idGen.matches("BOG-MED-[A-Z0-9]{8}"), "Formato esperado ok"+ idGen);
+    	
     }
 
     @Test
     @DisplayName("Debe lanzar excepción si origin es nulo")
     void shouldThrowWhenOriginIsNull() {
-        // TODO: implementar
-        fail("Test no implementado");
+        assertThrows(IllegalArgumentException.class, ()->generator.generate(null, "MED"));
+    }
+    
+    @Test
+    @DisplayName("Debe lanzar excepción si destination es nulo")
+    void shouldThrowWhenDestinationIsNull() {
+        assertThrows(IllegalArgumentException.class, ()->generator.generate("BOG", null));
+    }
+    
+    @DisplayName("Test parametrizado GENERATE correcto sin errores")
+    @ParameterizedTest
+    @CsvSource({"MED,BOG", "CAR,BOG", "MED,CAR"})
+    void parameterizedGenTest(String origin, String destination) throws Exception {
+
+    	String idGen = generator.generate(origin, destination);
+    	
+    	assertTrue(idGen.matches(origin+"-"+destination+"-[A-Z0-9]{8}"), "Formato esperado ok "+ idGen);
     }
 }
