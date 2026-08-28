054e33db8efd5db0b7ee30312d8093ef8adf94a2
Jesus Sierra <jmsierrav@indracompany.com>
2026-08-28 16:22:30 -0500

    Refactor: convertidas clases de modelo a Record

--- sessions/week-02-tdd-junit5/src/main/java/com/indra/transporte/model/Horario.java
+++ sessions/week-02-tdd-junit5/src/main/java/com/indra/transporte/model/Horario.java
@@ -2,14 +2,5 @@ package com.indra.transporte.model;
 
 import java.time.LocalTime;
 
-import lombok.Data;
-import lombok.ToString;
-
-@Data
-@ToString
-public class Horario {
-    private final Bus bus;
-    private final Ruta ruta;
-    private final LocalTime horaSalida;
-    private final LocalTime horaLlegada;
+public record Horario(Bus bus, Ruta ruta, LocalTime horaSalida, LocalTime horaLlegada) {
 }
--- sessions/week-02-tdd-junit5/src/main/java/com/indra/transporte/model/Ruta.java
+++ sessions/week-02-tdd-junit5/src/main/java/com/indra/transporte/model/Ruta.java
@@ -1,13 +1,4 @@
 package com.indra.transporte.model;
 
-import lombok.Data;
-import lombok.ToString;
-
-@Data
-@ToString
-public class Ruta {
-    private final String tipo;
-    private final String codigo;
-    private final String origen;
-    private final String destino;
+public record Ruta(String tipo, String codigo, String origen, String destino) {
 }
